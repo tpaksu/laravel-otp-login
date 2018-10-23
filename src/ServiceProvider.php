1e61f5095f426ae6f5d479ec99794b59a037ae2f
Taha Paksu <3295+tpaksu@users.noreply.github.com>
2018-10-23 17:56:42 +0300

    changed view paths

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -16,11 +16,12 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         $this->loadMigrationsFrom(__DIR__ . '/migrations');
         $this->loadRoutesFrom(__DIR__ . '/routes/routes.php');
         $this->loadViewsFrom(__DIR__ . '/views', 'laravel-otp-login');
+
         $this->publishes([__DIR__ . '/config/config.php' => config_path('otp-login.php')], "config");
-        $this->publishes([__DIR__.'/views' => resource_path('views')]);
+        $this->publishes([__DIR__.'/views/auth' => resource_path('views/vendor/laravel-otp-login')], "views");
         $this->publishes([__DIR__ . '/migrations' => database_path('migrations')], "migrations");
 
         $this->app['router']->pushMiddlewareToGroup('web', LoginMiddleware::class);
-        //dd($this->app['router']);
+
     }
 }
