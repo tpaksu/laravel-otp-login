0dc6c26f1339f0f77547904b964ca9d134050675
Taha Paksu <tpaksu@gmail.com>
2018-10-29 19:16:04 +0300

    updated missing view translations

--- src/views/otpvalidate.blade.php
+++ src/views/otpvalidate.blade.php
@@ -7,13 +7,13 @@
             <div class="text-center mb-6">
                 <img src="{{asset('assets/images/provas_male.png')}}" class="h-9" alt="">
             </div>
-            <form method="POST" action="{{ route('otp.verify') }}" class="card" aria-label="{{ __('Verify') }}">
+            <form method="POST" action="{{ route('otp.verify') }}" class="card" aria-label="{{ __('laravel-otp-login::messages.verify_phone_button') }}">
                 @csrf
                 <div class="card-body p-6">
-                    <div class="card-title">{{__("Verify Your Phone number")}}</div>
-					<p>{{__("Thanks for giving your details. An OTP has been sent to your Mobile Number. Please enter the :digit digit OTP below for Successful Login", ["digit" => config("otp.otp_digit_length", 6)])}}</p>
+                    <div class="card-title">{{__("laravel-otp-login::messages.verify_phone_title")}}</div>
+					<p>{{__("laravel-otp-login::messages.hero_text", ["digit" => config("otp.otp_digit_length", 6)])}}</p>
                     <div class="form-group">
-                        <label class="form-label">{{__("One-Time Password")}}</label>
+                        <label class="form-label">{{__("laravel-otp-login::messages.one_time_password")}}</label>
                         <input id="code" type="text" class="form-control{{ $errors->has('code') ? ' is-invalid' : '' }}" name="code" value="{{ old('code') }}"
                             required autofocus> @if ($errors->has('code'))
                         <span class="invalid-feedback" role="alert">
@@ -22,7 +22,7 @@
                         @endif
                     </div>
                     <div class="form-footer">
-                        <button type="submit" class="btn btn-primary btn-block">{{__("Verify Your Phone Number")}}</button>
+                        <button type="submit" class="btn btn-primary btn-block">{{__("laravel-otp-login::messages.verify_phone_button")}}</button>
                     </div>
                 </div>
             </form>
