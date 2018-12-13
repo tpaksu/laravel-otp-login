2e23f0fb0d72a7674afc71125b52608c3a5e7d2e
Taha Paksu <3295+tpaksu@users.noreply.github.com>
2018-12-13 17:45:38 +0300

    converted phone column to accept dot notated paths

--- src/services/BioTekno.php
+++ src/services/BioTekno.php
@@ -22,12 +22,14 @@ class BioTekno implements ServiceInterface
     }
     public function sendOneTimePassword(User $user, $otp)
     {
-        if (empty($user->phone)) return false;
+        // phone numbers need to be starting without a leading zero in this service
+        $user_phone = data_get($user, $this->phone_column, false);
+        if (!$user_phone) return false;
         try {
             $url = 'http://www.biotekno.biz:8080/SMS-Web/HttpSmsSend?' .
                 'Username=' . $this->username .
                 '&Password=' . $this->password .
-                '&Msisdns=' . $user->{$this->phone_column} .
+                '&Msisdns=' . $user_phone .
                 '&TransmissionID=' . $this->transmissionID .
                 '&Messages=' . urlencode(iconv("UTF-8", "ASCII//TRANSLIT", str_replace(":password", $otp, $this->message)));
             $results = file_get_contents($url);
